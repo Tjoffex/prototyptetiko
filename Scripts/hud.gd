6d4f9eb979bec4f04e1009cbb150fa5c6bc115b2
Tjoffex <tjoffex@gmail.com>
2025-07-01 14:53:28 +0200

    Pause function and menu
    Pause/unpause main process, HUD-menu relocalised

--- Scripts/hud.gd
+++ Scripts/hud.gd
@@ -5,9 +5,19 @@ extends Node2D
 func _ready() -> void:
 	_crosshair.play("default")
 
+
+
 func _on_player_character_trigger_pulled() -> void:
 	_crosshair.play("trigger_held")
 
 func _on_player_character_trigger_released() -> void:
 	_crosshair.play("default")
 	#trigger enemy hitcheck
+
+func _on_world_exit() -> void:
+	%MainMenu.visible = true
+	_crosshair.visible = false
+
+func _on_resume_button_pressed() -> void:
+	%MainMenu.visible = false
+	_crosshair.visible = true
